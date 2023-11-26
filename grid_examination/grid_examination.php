98898c7f5f19e2047de6c9a8ae7e2ae942676e93
jaisyullah <jais.rafi@gmail.com>
2023-11-27 05:22:50 +0700

    initial commit

---
+++ grid_examination/grid_examination.php
@@ -0,0 +1,3 @@
+<?php 
+  include_once('../grid_examination/index.php'); 
+?> 
---
+++ grid_examination/grid_examination_lookup.class.php
@@ -0,0 +1,48 @@
+<?php
+class grid_examination_lookup
+{
+//  
+   function lookup_dpjp(&$conteudo , $dpjp) 
+   {   
+      static $save_conteudo = "" ; 
+      static $save_conteudo1 = "" ; 
+      $tst_cache = $dpjp; 
+      if ($tst_cache === $save_conteudo && $conteudo != "&nbsp;") 
+      { 
+          $conteudo = $save_conteudo1 ; 
+          return ; 
+      } 
+      $save_conteudo = $tst_cache ; 
+      if (trim($dpjp) === "" || trim($dpjp) == "&nbsp;")
+      { 
+          $conteudo = "&nbsp;";
+          $save_conteudo  = ""; 
+          $save_conteudo1 = ""; 
+          return ; 
+      } 
+      $nm_comando = "select name from practitioner where id = $dpjp order by name" ; 
+      $conteudo = "" ; 
+      $_SESSION['scriptcase']['sc_sql_ult_comando'] = $nm_comando; 
+      $_SESSION['scriptcase']['sc_sql_ult_conexao'] = ''; 
+      if ($rx = $this->Db->Execute($nm_comando)) 
+      { 
+          if (isset($rx->fields[0]))  
+          { 
+              $conteudo = trim($rx->fields[0]); 
+          } 
+          $save_conteudo1 = $conteudo ; 
+          $rx->Close(); 
+      } 
+      elseif ($GLOBALS["NM_ERRO_IBASE"] != 1)  
+      { 
+          $this->Erro->mensagem(__FILE__, __LINE__, "banco", $this->Ini->Nm_lang['lang_errm_dber'], $this->Db->ErrorMsg()); 
+          exit; 
+      } 
+      if ($conteudo === "") 
+      { 
+          $conteudo = "&nbsp;";
+          $save_conteudo1 = $conteudo ; 
+      } 
+   }  
+}
+?>
